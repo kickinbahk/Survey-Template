3e344c8e0d6ef61d1f74892dc38c80735e7d9877
Nathan Fennel <nathanfennel@gmail.com>
2016-04-22 13:18:11 +0200

    Initial Commit

---
+++ Survey Template/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Survey Template
+//
+//  Created by HAI on 4/22/16.
+//  Copyright © 2016 Nathan Fennel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Survey Template/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Survey Template
+//
+//  Created by HAI on 4/22/16.
+//  Copyright © 2016 Nathan Fennel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
