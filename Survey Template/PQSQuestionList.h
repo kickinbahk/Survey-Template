d14c2708c8fd58fe311c81578e1d4443d9b511a3
Nathan Fennel <nathanfennel@gmail.com>
2016-06-15 14:16:14 -0400

    Created Questions List class to help simplify creating new surveys

---
+++ Survey Template/PQSQuestionList.h
@@ -0,0 +1,13 @@
+//
+//  PQSQuestionList.h
+//  Survey Template
+//
+//  Created by HAI on 6/15/16.
+//  Copyright © 2016 Nathan Fennel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PQSQuestionList : NSMutableArray
+
+@end
---
+++ Survey Template/PQSQuestionList.m
@@ -0,0 +1,13 @@
+//
+//  PQSQuestionList.m
+//  Survey Template
+//
+//  Created by HAI on 6/15/16.
+//  Copyright © 2016 Nathan Fennel. All rights reserved.
+//
+
+#import "PQSQuestionList.h"
+
+@implementation PQSQuestionList
+
+@end
